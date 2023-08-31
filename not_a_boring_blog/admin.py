200036c575489a5abda65b2a509480efda0ddf4d
Aias21 <alexandru.serban21@gmail.com>
2023-08-31 17:28:17 +0200

    created Role and RepostRequest model, corrected model name where it was called, added related_name where there were more than one FK. TO DO We need to be able to select multiple categories

--- not_a_boring_blog/admin.py
+++ not_a_boring_blog/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models.comment import Comment, ReplyComment
 from .models.post import Post, Category
+from .models.user import Role
+
 
 admin.site.register(Comment)
 admin.site.register(ReplyComment)
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Role)
--- not_a_boring_blog/models/comment.py
+++ not_a_boring_blog/models/comment.py
@@ -1,20 +1,22 @@
 from django.db import models
 from .post import Post
-from .user import User
+from .user import Role
+
 
 class Comment(models.Model):
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_id')
     body = models.CharField(max_length=500)
-    following_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Role, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)    
 
     def __str__(self):
         return self.body[:50]    
 
+
 class ReplyComment(models.Model):
-    comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='comment_id')
     body = models.CharField(max_length=500)
-    following_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Role, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)     
 
     def __str__(self):
