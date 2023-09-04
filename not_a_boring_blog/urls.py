28d684e7d2148e567c298f75bc09ad2e48875572
AknetN <aknet52@gmail.com>
2023-09-05 01:16:40 +0200

    tried to work work with html, but runserver has an error

--- not_a_boring_blog/urls.py
+++ not_a_boring_blog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from .views.views import home
-from .views.list_post_view import PostListCreateView, PostDetailView
+from .views.list_post_view import PostListCreateView, PostDetailView, ArticleListView
 
 
 app_name = "not_a_boring_blog"
@@ -8,4 +8,8 @@ urlpatterns = [
     path('home/', home, name='home'),
     path('posts/', PostListCreateView.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    #path('articles/', article_list, name='article-list'),
+    #path('articles/', include('not_a_boring_blog.urls')),
+    path('articles/', ArticleListView.as_view(), name='article-list'),
+
     ]
--- not_a_boring_blog/views/list_post_view.py
+++ not_a_boring_blog/views/list_post_view.py
@@ -1,33 +1,14 @@
-# # pip install djangorestframework
-
-
-
-# class BlogList(generics.ListAPIView):
-#     queryset = models.Post.objects.filter(user_id = self.request.user.id)
-#     serializer_class = serializers.PostSerializer
-
-#     def get_queryset(self):
-#         return Post.objects.filter(user_id=self.kwargs['user_id'])
-
-
-# from rest_framework import generics
-# from .models import Post
-# from .serializers import PostSerializer
-
-# class PostList(generics.ListCreateAPIView):
-#     queryset = Post.objects.all()  # You can adjust this queryset as needed.
-#     serializer_class = PostSerializer
-
-#     def perform_create(self, serializer):
-#         serializer.save(user_id=self.request.user)  # Set the user when creating a post
-
-
-
 from rest_framework import generics
 from rest_framework import permissions
 from ..models.post import Post
 from ..serializers.serializers import PostSerializer
 from .permissions import IsOwnerOrReadOnly
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django.views import View
+
+
 
 # for listing and creating posts
 class PostListCreateView(generics.ListCreateAPIView):
@@ -45,4 +26,18 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
 
 
+# class ArticleListView(View):
+#     def get(self, request):
+#         articles = Post.objects.all()
+#         template = loader.get_template('not_a_boring_blog/html/list_post.html')  # Corrected template path
+#         context = {'articles': articles}
+#         return HttpResponse(template.render(context, request))
 
+
+class ArticleListView(View):
+    template_name = 'list_post.html'  # Specify the template path
+
+    def get(self, request):
+        articles = Post.objects.all()
+        context = {'articles': articles}
+        return render(request, self.template_name, context)
