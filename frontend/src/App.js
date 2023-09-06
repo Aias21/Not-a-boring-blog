596db92e6c2a8c96ef5f3cf06a1357ba5d58d77c
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-09-06 16:00:12 +0200

    add blog template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Registration from './components/Registration';
+import Blog from './components/Blog';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
-        {/* Add other routes as needed */}
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </Router>
   );
