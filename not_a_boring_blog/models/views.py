b567c5bdabf326df5fb493de7a879f8cca07896c
Dejan <dejanbrnada@gmail.com>
2023-09-09 12:16:14 +0200

    updated

--- not_a_boring_blog/models/views.py
+++ not_a_boring_blog/models/views.py
@@ -1,15 +1,6 @@
 from django.db import models
 from .user import Role
 from .post import Post
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework import permissions
-from datetime import datetime, timedelta
-
-
-
-COOLDOWN_PERIOD = timedelta(minutes=5)
 
 
 class View(models.Model):
