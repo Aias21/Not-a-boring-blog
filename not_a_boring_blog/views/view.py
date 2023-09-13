10f63b51f0c4810d1b0e050cdb050253e24e208f
shimzofremnik <98681891+shimzofremnik@users.noreply.github.com>
2023-09-13 09:36:16 +0200

    Update view.py
    updated timecooldown

--- not_a_boring_blog/views/view.py
+++ not_a_boring_blog/views/view.py
@@ -8,27 +8,20 @@ from rest_framework import permissions
 from datetime import datetime, timedelta, timezone
 
 
-COOLDOWN_PERIOD = timedelta(minutes=5)
-
-
 @api_view(['POST'])
 @permission_classes([permissions.IsAuthenticated])
 def record_post_view(request, post_id):
     user = request.user
     post = get_object_or_404(Post, pk=post_id)
-    print(user)
+    cooldown_period = timedelta(minutes=int(post.min_read))
 
     if post.user_id == user.id:
         return Response({"message": "Author's own view is not counted"})
 
     last_view = View.objects.filter(post_id=post.id, user_id=user.id).order_by('-timestamp').first()
-    print(datetime.now())
-    #vrijeme = last_view.timestamp.strptime("%Y-%m-%d %H:%M:%S")
-    #datetime_object = datetime.strptime(last_view.timestamp, "%Y-%m-%d %H:%M:%S")
-    #print(type(last_view.timestamp))
+   
     if not last_view or (datetime.now(timezone.utc) - last_view.timestamp) > COOLDOWN_PERIOD:
         role = get_object_or_404(Role, id=user.id)
-        print(role)
         View.objects.create(post_id=post, user_id=role)
 
         return Response({"message": "View recorded successfully"})
