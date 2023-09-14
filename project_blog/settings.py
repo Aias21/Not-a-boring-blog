832866f6532f4bcc0633c9c824e72aafea865d8f
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-09-14 11:14:55 +0200

    add validation for user exist and email exist for frontend fetching

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'not_a_boring_blog',
     'rest_framework',
     'rest_framework.authtoken',
-    # 'corsheaders',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -64,7 +64,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
