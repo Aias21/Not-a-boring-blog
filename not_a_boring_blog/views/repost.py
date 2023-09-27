d74952d9e8a2d9db280d3f9b9da416fb89e7aea5
AknetN <aknet52@gmail.com>
2023-09-27 15:03:00 +0200

    created tests for repost views

--- not_a_boring_blog/views/repost.py
+++ not_a_boring_blog/views/repost.py
@@ -40,8 +40,7 @@ class CreateRepostRequest(APIView):
             # Create the repost request
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RepostRequestedReceivedList(APIView):
