c044dc7abace5ce80e130510b57a066c5a7ce2c1
AknetN <aknet52@gmail.com>
2023-09-28 15:57:55 +0200

    changed CreateCategory view doc

--- not_a_boring_blog/views/category.py
+++ not_a_boring_blog/views/category.py
@@ -8,19 +8,30 @@ from rest_framework.permissions import IsAuthenticated
 
     
 class CreateCategory(APIView):  
-    """This API creates a post category.
-    Requirements:
-    - The user must be authenticated to create a category.
-    - The category name provided should not yet exist in the database.
+    """***This API creates a new post category.***
     
-    How to use:
-    - Make a POST request to the API endpoint.
-    - Attach the authentication token in the header to ensure you're authenticated. 
-    The token should belong to an authenticated user.
-    - In the request body, provide the category name you wish to create in the format:
-        {"category_name": "Your Category Name Here"}
-    - If successful, the API will return a success message along with the category ID. 
-    If there are any errors, appropriate error messages will be returned.
+    <b>Requirements</b>:
+    - The user must be registered and authenticated to create a category.
+    - The user will need to use their token 
+    - The category name you provide should not yet exist in the database.<p>
+    ------------------------------------------------------------<p>
+    ***HOW TO USE:***<p>
+    <ul><b>1. AUTHENTICATION</b><p>
+    <ul><b>1.1.</b>  Before making a request to this endpoint, ensure that you are authenticated, using your token. <p>
+    ---> For this check <i><u>user/registration/</u></i> and <i><u>user/login</u></i>.<p>    
+    <b>1.2.</b> Apply the token, it should belong to the authenticated user.<p>
+    !!! For this follow the steps:<p>
+    ---> click on the image of a <b>lock</b> in the right corner of your highlighted box, <p> 
+    ---> choose <b><i>tokenAuth</i></b>,<p> 
+    ---> insert your <b><i>token key</i></b> and <b>Authorize</b></ul></ul><p>
+    ------------------------------------------------------------<p>
+    <ul><b>2. BODY</b>:<p>
+    <ul><b>2.1.</b> In order to add <b>json</b> information to the <b>body</b>, click on <b><i>Try it out</i></b> button<p>
+    <b>2.2.</b> In the request body (application/json), provide a unique category name you wish to create in the format:<p>
+        <b><i>{"category_name": "Your Category Name Here"}</i></b><p>
+    <b>2.3.</b>  Press the <b><i>Execute</i></b> button in order to send a <b>POST</b> request to the API endpoint.<p>
+    -- If successful, the API will return a success message along with <i><u>detail</u></i> and <i><u>category_id</u></i>. <p>
+    -- If there are any errors, appropriate error messages will be returned.</ul></ul>
     """ 
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
